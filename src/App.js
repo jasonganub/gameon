1967ddfc4d00f9b2803cddae6c5af37b7d3cdf9c
Jason Ganub <jasonganub@gmail.com>
2021-07-24 11:17:03 +0800

    Make axios pokemon api call and display names with url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
-import Game from './utils/game'
+import axios from 'axios';
+import React from 'react';
+import Pokemon from './utils/pokemon'
+
+const baseURL = 'https://pokeapi.co/api/v2'
+const getAllPokemon = baseURL + '/pokemon?limit=151'
 
 function App() {
+  const [pokemonList, setPokemonList] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(getAllPokemon).then((response) => {
+      setPokemonList(response.data.results);
+    })
+  })
+
+  if (!pokemonList) return null;
+
+  const pokemonItems = pokemonList.map((pokemon) => 
+        <li>
+          {pokemon.name}
+          <br></br>
+          {pokemon.url}
+        </li>
+  )
+
   return (
-    "HI"
-    // <Game name="Catan" imageURL="https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254204253-61te4F7OK2BL.jpg"/>
+    <div>
+      <h1>Pokedex</h1>
+      <ul>{pokemonItems}</ul>
+    </div>
   );
 }
 
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
 
-function Game() {
+function Pokemon() {
     const { name, imageURL } = this.props;
 
-    const gameDetails = (
+    const pokemon = (
         <div>
-            <h1>catan</h1>
-            <img src="" alt=""></img>
+            <h1>{name}</h1>
+            <img src={imageURL} alt=""></img>
         </div>
     )
     return (
         <div
             className="mx-auto mt-4 text-center p-5"
         >
-            {gameDetails}
+            {pokemon}
         </div>
     )
 }
 
-Game.propTypes = {
+Pokemon.propTypes = {
     name: PropTypes.string,
     imageURL: PropTypes.string
 }
 
-export default Game;
+export default Pokemon;
