7b7eb97895541bc578d1395b6abafc7056507f39
Jason Ganub <jasonganub@gmail.com>
2021-07-31 08:27:45 +0700

    Clean up get all pokemon API call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Pokemon from './utils/pokemon'
 
 const baseURL = 'https://pokeapi.co/api/v2'
@@ -8,31 +8,22 @@ const getAllPokemon = baseURL + '/pokemon?limit=151'
 
 function App() {
   const [pokemonList, setPokemonList] = React.useState(null);
-  let pokemonWithDetails = [];
 
-  React.useEffect(() => {
-    axios.get(getAllPokemon).then((response) => {
-      setPokemonList(response.data.results);
-    })
+  useEffect(() => {
+    axios
+      .get(getAllPokemon)
+      .then((res) => {
+        setPokemonList(res.data.results);
+      })
   })
 
   if (!pokemonList) return null;
-
-  pokemonList.map((pokemon) => 
-    axios.get(pokemon.url).then((response) => {
-      pokemonWithDetails.push(response.data.name)
-    })
-  )
-
-  if (!pokemonWithDetails) return null;
-
-  console.log(pokemonWithDetails)
-
-  const pokemonItems = pokemonWithDetails.map((pokemon) => 
-        <li>
-          {pokemon.name}
-          <br></br>
-        </li>
+  
+  const pokemonItems = pokemonList.map((pokemon) =>
+    <li key={pokemon.name}>
+      {pokemon.name}
+      <br></br>
+    </li>
   )
 
   return (
