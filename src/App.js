94228fb28452ff0bd7c8232d5da9ab811f3e6ea1
Jason Ganub <jasonganub@gmail.com>
2021-07-31 17:55:06 +0700

    Stop infinite loop by passing in empty array to useEffect

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ const getAllPokemon = baseURL + '/pokemon?limit=500'
 function PokemonItems() {
   const [pokemonList, setPokemonList] = useState(null);
 
+  // useEffect runs after the initial render which causes re-render infinite loop
   useEffect(() => {
     fetch(getAllPokemon)
       .then((res) => res.json())
       .then((data) => {
         setPokemonList(data.results);
-        return
       })
-  })
+  }, [])
 
   if (!pokemonList) return null;
 
