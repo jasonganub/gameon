2c516a8bd2cb32e7bd517cbca4b27a61c119f4e7
Jason Ganub <jasonganub@gmail.com>
2021-07-31 23:10:04 +0700

    Create promise all for all get pokemon API request

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import PokemonItems from "./utils/pokemonItems"
 import { ChakraProvider, Box, Image, Center } from "@chakra-ui/react"
 
 function App() {
+  const pokemon = PokemonItems()
+
   return (
     <ChakraProvider>
       <Box w="100%" p={4} color="white">
         <Center>
         <Image align="center" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" />
         </Center>
-        {PokemonItems()}
+        {pokemon}
       </Box>
     </ChakraProvider>
   );
--- src/utils/pokemonItems.js
+++ src/utils/pokemonItems.js
@@ -2,11 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { Box, Image } from "@chakra-ui/react"
 
 const baseURL = 'https://pokeapi.co/api/v2/pokemon'
-const getAllPokemon = baseURL + '?limit=500'
+const getAllPokemon = baseURL + '?limit=5'
 
 function Pokemon(pokemon) {
-    console.log(pokemon)
-
     return (
         <Box w="100%">
             <Image alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`} width="200px" height="200px" />
@@ -33,41 +31,42 @@ function GetPokemon(name) {
     return new Promise((resolve, reject) => {
         fetch(`${baseURL}/${name}`)
             .then((res) => {
-              resolve(res.json())  
+                resolve(res.json())
             })
     })
 }
 
 function PokemonItems() {
-    const [pokemonList, setPokemonList] = useState(null);
+    const [pokemonNames, setPokemonNames] = useState(null);
+    const [pokemonData, setPokemonData] = useState(null);
 
     // useEffect runs after the initial render which causes re-render infinite loop
     useEffect(() => {
         fetch(getAllPokemon)
             .then((res) => res.json())
             .then((data) => {
-                setPokemonList(data.results.map(pokemon => {
+                setPokemonNames(data.results.map(pokemon => {
                     return pokemon.name
                 }));
             })
     }, [])
 
-    if (!pokemonList) return null;
+    if (!pokemonNames) return null;
 
     let pokemonRequests = []
-    pokemonList.forEach(
+    pokemonNames.forEach(
         (name) => {
             pokemonRequests.push(GetPokemon(name))
         }
     )
-
+    
     Promise.all(pokemonRequests).then((allPokemonData) => {
-        allPokemonData.forEach(
-            (pokemon) => {
-                return Pokemon(pokemon)
-            }
-        )
+        setPokemonData(allPokemonData.map(pokemon => {
+            return Pokemon(pokemon)
+        }))
     })
+
+    return pokemonData
 }
 
 export default PokemonItems;
