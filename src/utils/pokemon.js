af9fcb25bd3cccf439ddb03d265c3c4f72d53f4b
Jason Ganub <jasonganub@gmail.com>
2021-08-01 13:17:55 +0700

    Add abilities to pokemon

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -18,27 +18,25 @@ function Pokemon(pokemon) {
                         )
                     })}
 
-                    <Box
-                        color="gray.500"
-                        fontWeight="semibold"
-                        letterSpacing="wide"
-                        fontSize="xs"
-                        textTransform="uppercase"
-                        ml="2"
-                    >
+                    <Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase" ml="2">
                         {pokemon.height * 10} cm &bull; {pokemon.weight / 10} kg
                     </Box>
                 </Box>
 
-                <Box
-                    mt="1"
-                    fontWeight="semibold"
-                    as="h4"
-                    lineHeight="tight"
-                    isTruncated
-                    color="black"
-                >
+                <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated color="black">
                     {pokemon.name}
+                </Box>
+
+                <Box as="span" color="gray.600" fontSize="sm">
+                    {pokemon.abilities.map((value, i) => {
+                        const ability = value.ability.name
+                        if (pokemon.abilities.length === i + 1) {
+                            return <span>{ability}</span>
+                        } else {
+                            return <span>{ability} &bull; </span>
+                        }
+
+                    })}
                 </Box>
             </Box>
         </Box>
