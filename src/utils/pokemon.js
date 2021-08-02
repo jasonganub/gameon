d83d2b5fcbac46f9f659b237b163e79e300970f0
Jason Ganub <jasonganub@gmail.com>
2021-08-02 10:10:33 +0700

    Make each pokemon box a hyperlink to more information

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -3,42 +3,44 @@ import Colors from "./types"
 
 function Pokemon(pokemon) {
     return (
-        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2">
-            <Image pt="6" pl="6" alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`} width="200px" height="200px" />
+        <a href={`https://www.pokemon.com/us/pokedex/${pokemon.name}`}>
+            <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2">
+                <Image pt="6" pl="6" alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`} width="200px" height="200px" />
 
-            <Box p="6">
-                <Box d="flex" alignItems="baseline">
-                    {pokemon.types.map((value, _) => {
-                        const type = value.type.name
-                        const color = Colors[type]
-                        return (
-                            <Badge borderRadius="full" px="2" backgroundColor={color} color="white" mr="1">
-                                {type}
-                            </Badge>
-                        )
-                    })}
-                </Box>
-                <Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase" pt="1">
+                <Box p="6">
+                    <Box d="flex" alignItems="baseline">
+                        {pokemon.types.map((value, _) => {
+                            const type = value.type.name
+                            const color = Colors[type]
+                            return (
+                                <Badge borderRadius="full" px="2" backgroundColor={color} color="white" mr="1">
+                                    {type}
+                                </Badge>
+                            )
+                        })}
+                    </Box>
+                    <Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase" pt="1">
                         {pokemon.height * 10} cm &bull; {pokemon.weight / 10} kg
                     </Box>
 
-                <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated color="black">
-                    {pokemon.name}
-                </Box>
+                    <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated color="black">
+                        {pokemon.name}
+                    </Box>
 
-                <Box as="span" color="gray.600" fontSize="sm">
-                    {pokemon.abilities.map((value, i) => {
-                        const ability = value.ability.name
-                        if (pokemon.abilities.length === i + 1) {
-                            return <span>{ability}</span>
-                        } else {
-                            return <span>{ability} &bull; </span>
-                        }
+                    <Box as="span" color="gray.600" fontSize="sm">
+                        {pokemon.abilities.map((value, i) => {
+                            const ability = value.ability.name
+                            if (pokemon.abilities.length === i + 1) {
+                                return <span>{ability}</span>
+                            } else {
+                                return <span>{ability} &bull; </span>
+                            }
 
-                    })}
+                        })}
+                    </Box>
                 </Box>
             </Box>
-        </Box>
+        </a>
     )
 }
 
