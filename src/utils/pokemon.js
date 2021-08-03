443dc645cc2e36efb8ab16f7c39e6382b32d03cf
Jason Ganub <jasonganub@gmail.com>
2021-08-03 07:49:19 +0700

    Refactor certain boxes to be inline

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -12,20 +12,14 @@ function Pokemon(pokemon) {
                         {pokemon.types.map((value, _) => {
                             const type = value.type.name
                             const color = Colors[type]
-                            return (
-                                <Badge borderRadius="full" px="2" backgroundColor={color} color="white" mr="1">
-                                    {type}
-                                </Badge>
-                            )
+                            return <Badge borderRadius="full" px="2" backgroundColor={color} color="white" mr="1">{type}</Badge>
                         })}
                     </Box>
                     <Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase" pt="1">
                         {pokemon.height * 10} cm &bull; {pokemon.weight / 10} kg
                     </Box>
 
-                    <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated color="black">
-                        {pokemon.name}
-                    </Box>
+                    <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated color="black">{pokemon.name}</Box>
 
                     <Box as="span" color="gray.600" fontSize="sm">
                         {pokemon.abilities.map((value, i) => {
