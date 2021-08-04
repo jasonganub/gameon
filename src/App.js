f905a8a7795d1688bc9b2cdfbc11633fe6447470
Jason Ganub <jasonganub@gmail.com>
2021-08-04 22:59:15 +0700

    Refactor app to do all fetching

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import './App.css';
-import PokemonItems from "./utils/pokemonItems"
+import { useEffect, useState } from 'react';
+import Pokemon from './utils/pokemon'
 import { ChakraProvider, Box, Image, Center, SimpleGrid } from "@chakra-ui/react"
 
+const baseURL = 'https://pokeapi.co/api/v2/pokemon'
+const getAllPokemon = baseURL + '?limit=151'
+
+function GetPokemon(name) {
+  return new Promise((resolve, reject) => {
+      fetch(`${baseURL}/${name}`)
+          .then((res) => {
+              resolve(res.json())
+          })
+  })
+}
+
 function App() {
-  const pokemon = PokemonItems()
+  const [names, setNames] = useState([]);
+  const [data, setData] = useState([]);
+
+  // useEffect runs after the initial render which causes re-render infinite loop
+  useEffect(() => {
+    fetch(getAllPokemon)
+      .then((res) => res.json())
+      .then((data) => {
+        setNames(data.results.map(pokemon => {
+          return pokemon.name
+        }));
+      })
+  }, [])
+
+  useEffect(() => {
+    let pokemonRequests = []
+    names.forEach(
+      (name) => {
+        pokemonRequests.push(GetPokemon(name))
+      }
+    )
+
+    Promise.all(pokemonRequests).then((allPokemonData) => {
+      setData(allPokemonData.map(pokemon => {
+        return Pokemon(pokemon)
+      }))
+    })
+  }, [names])
 
   return (
     <ChakraProvider>
       <Box w="100%" p={4} color="white">
         <Center>
-        <Image pb="5" align="center" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" />
+          <Image pb="5" align="center" src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" />
         </Center>
 
         <SimpleGrid minChildWidth="240px" spacing="20px">
-        {pokemon}
+          {data}
         </SimpleGrid>
       </Box>
     </ChakraProvider>
